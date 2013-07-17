dfc3fdaf093d7f7a74c92152662d1c83e0958de9
Kelsey Hightower <kelsey@puppetlabs.com>
2013-07-17 06:12:23 -0700

    Add license and comments for the network interfaces example

--- network-interfaces.go
+++ network-interfaces.go
@@ -1,3 +1,16 @@
+// Copyright 2013 Kelsey Hightower
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package main
 
 import (
@@ -13,6 +26,8 @@ type NetworkInterface struct {
 	Addrs      []string `json:"ipaddress"`
 }
 
+// NetworkInterfacesToMap returns a mapping of network interface names to
+// interface attributes.
 func NetworkInterfacesToMap() (networkInterfaceMap map[string]NetworkInterface) {
 	networkInterfaceMap = make(map[string]NetworkInterface)
 
@@ -39,6 +54,8 @@ func NetworkInterfacesToMap() (networkInterfaceMap map[string]NetworkInterface) 
 
 func main() {
 	networkInterfaceMap := NetworkInterfacesToMap()
+
+	// Use json.MarshalIndent() so we get pretty json output of our map
 	networkInterfaceJSON, err := json.MarshalIndent(networkInterfaceMap, "", "  ")
 	if err != nil {
 		log.Fatal(err.Error())
